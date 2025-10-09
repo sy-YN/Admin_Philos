0cc25e2ccf403bb77632181f47569ce786be59ec
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-10-09 06:47:26 +0000

    つながってなさそうです...。

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "YOUR_API_KEY",
+  authDomain: "studio-8638705182-2858a.firebaseapp.com",
+  projectId: "studio-8638705182-2858a",
+  storageBucket: "studio-8638705182-2858a.appspot.com",
+  messagingSenderId: "1073866162334",
+  appId: "1:1073866162334:web:7f6d43ea56f1406180c576",
 };
 
 // Initialize Firebase
