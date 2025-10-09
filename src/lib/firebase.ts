e1f382b745a2f380e8f0f3940d512423cf659ded
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-10-09 06:48:46 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "studio-8638705182-2858a.firebaseapp.com",
-  projectId: "studio-8638705182-2858a",
-  storageBucket: "studio-8638705182-2858a.appspot.com",
-  messagingSenderId: "1073866162334",
-  appId: "1:1073866162334:web:7f6d43ea56f1406180c576",
+  apiKey: "AIzaSyC9oIHt72gUTxiXYwDTgsNxHgzVTfEcedI",
+  authDomain: "philos2-41336736-69b80.firebaseapp.com",
+  projectId: "philos2-41336736-69b80",
+  storageBucket: "philos2-41336736-69b80.appspot.com",
+  messagingSenderId: "97381895941",
+  appId: "1:97381895941:web:3e4f51df6cd2ac10a4716c",
 };
 
 // Initialize Firebase
