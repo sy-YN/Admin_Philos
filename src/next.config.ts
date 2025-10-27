b880794ce4c4800a3cab869263bb10a25cbffe70
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-10-27 04:12:00 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,45 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: (process.env.NEXT_PUBLIC_FUNCTIONS_EMULATOR_URL || 'http://127.0.0.1:5001/studio-8638705182-2858a/us-central1') + '/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
