924a2acc76df26f01c4218ea3db3f8157a80100e
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-11-26 00:57:07 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
+  // In development, do not apply any middleware logic.
   if (process.env.NODE_ENV === 'development') {
     return NextResponse.next();
   }
@@ -14,7 +15,8 @@ export function middleware(req: NextRequest) {
 
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(authValue).split(':');
+    // This will be undefined if the authValue is not a valid base64 string
+    const [user, pwd] = atob(authValue)?.split(':') ?? [];
 
     if (
       user === process.env.BASIC_AUTH_USER &&
@@ -23,6 +25,8 @@ export function middleware(req: NextRequest) {
       return NextResponse.next();
     }
   }
+  
+  // If we've reached here, authentication has failed.
   url.pathname = '/api/auth';
 
   return NextResponse.rewrite(url);
