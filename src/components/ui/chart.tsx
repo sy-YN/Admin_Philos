2967c44cd185bd1575a06c3560f4eec037d80012
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-12-12 06:38:56 +0000

    色と四角は出ていますが、何が何なのかが示されておらず和化rにくいです

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -270,8 +270,6 @@ const ChartLegendContent = React.forwardRef<
     { className, hideIcon = false, payload, verticalAlign = "bottom", nameKey },
     ref
   ) => {
-    const { config } = useChart()
-
     if (!payload?.length) {
       return null
     }
@@ -286,27 +284,20 @@ const ChartLegendContent = React.forwardRef<
         )}
       >
         {payload.map((item) => {
-          const key = `${nameKey || item.dataKey || "value"}`
-          const itemConfig = getPayloadConfigFromPayload(config, item, key)
-
           return (
             <div
-              key={item.value}
+              key={item.value as string}
               className={cn(
                 "flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3 [&>svg]:text-muted-foreground"
               )}
             >
-              {itemConfig?.icon && !hideIcon ? (
-                <itemConfig.icon />
-              ) : (
-                <div
-                  className="h-2 w-2 shrink-0 rounded-[2px]"
-                  style={{
-                    backgroundColor: item.color,
-                  }}
-                />
-              )}
-              {itemConfig?.label}
+              <div
+                className="h-2 w-2 shrink-0 rounded-[2px]"
+                style={{
+                  backgroundColor: item.color,
+                }}
+              />
+              {item.value}
             </div>
           )
         })}
